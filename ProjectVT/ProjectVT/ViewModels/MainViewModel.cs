926161b060b644d7f0c2c84c9dfbcab1364054ff
Kozzoko <phenxskyzz@gmail.com>
2025-06-10 18:02:06 +0200

    Rebase

--- ProjectVT/ProjectVT/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ProjectVT.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- ProjectVT/ProjectVT/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace ProjectVT.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
